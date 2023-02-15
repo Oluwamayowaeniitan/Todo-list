0fb9cf20c374aa79dfb1f1486a591d93a29cbed9
Oluwamayowaeniitan <113830623+Oluwamayowaeniitan@users.noreply.github.com>
2023-02-15 10:55:35 +0100

    Add files via upload
    My todo list

---
+++ todolist.js
@@ -0,0 +1,11 @@
+// console.log(document.querySelectorAll("li"))
+// document.querySelectorAll("li")[1].setAttribute('class','new');
+// document.querySelectorAll("li")[0].className="new";
+// document.querySelectorAll("li")[0].classList.toggle("new");
+//ADDING A NEW LIST ELEMENT TO A WEB PAGE FROM JS
+
+
+// var p = document.querySelector("p");
+// p.innerHTML = "<strong>MAYOR</strong>";
+// var a = document.querySelector("a");
+// a.innerText = "click here";
